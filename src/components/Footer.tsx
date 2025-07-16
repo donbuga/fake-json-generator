1f14a4c69a8f3d2e3ce26e7af7eaa774e85e60f7
buga-dev <bugadj@gmail.com>
2025-07-16 17:23:46 -0400

    mod: Footer.tsx link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="py-4 text-center text-sm text-gray-600 dark:text-gray-400">
-      Otras utilidades{' '}
-      <a
-        href="https://donbuga.github.io/rutGenerator/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500 hover:underline"
-      >
-        RUT Generator
-      </a>
-    </footer>
+   <footer className="bg-gray-200 text-center py-4 mt-6">
+     <p className="text-sm text-gray-600">Fake json generator</p>
+     <p className="text-sm text-gray-600">
+       Otras utilidades:{' '}
+       <a
+         href="https://donbuga.github.io/rutGenerator/"
+         className="text-blue-500 hover:underline"
+       >
+         RUT Generator
+       </a>
+     </p>
+   </footer>
   );
 };
 
